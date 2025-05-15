02423cbeee19519ca53e23cb8ae5fb7bb32e5d43
Ibrahim <ibrahimelothmani98@gmail.com>
2025-05-14 18:42:30 -1200

    trying the ORDER BY , DESC and ASC

--- 04-query-data.sql
+++ 04-query-data.sql
@@ -1,6 +1,12 @@
 -- Query data from the events table
 -- SELECT * FROM events;
 
+-- in this chapter we will learn about filtering data using WHERE clause
+
+-- SELECT * FROM events;
+-- WHERE date_planned >= '2023-10-01 00:00:00' 
+-- AND min_age >= 25;
+
 SELECT * FROM events
-WHERE date_planned >= '2023-10-01 00:00:00' 
-AND min_age >= 25;
+ORDER BY id DESC -- By default they sorts in ascending order so we can descending them using DESC
+
