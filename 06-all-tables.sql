94a017ba412fcee93a4255041ed40d9268598136
Ibrahim <ibrahimelothmani98@gmail.com>
2025-05-15 04:46:17 -1200

    create all the tables and focusing on the ManyToMany Relation

--- 06-all-tables.sql
+++ 06-all-tables.sql
@@ -7,8 +7,8 @@ CREATE TABLE locations (
     title VARCHAR(300),
     street VARCHAR(300) NOT NULL,
     house_number VARCHAR(10) NOT NULL,
-    postal_code VARCHAR(5) NOT NULL?
-    city_name VARCHAR(300) NOT NULL REFERENCES cities ON DELETE RESTRICT CASCADE ON UPDATE
+    postal_code VARCHAR(5) NOT NULL,
+    city_name VARCHAR(300) NOT NULL REFERENCES cities ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE users (
@@ -21,7 +21,7 @@ CREATE TABLE users (
 
 CREATE TABLE organizers (
     password VARCHAR(100) NOT NULL,
-    user_id INT PRIMARY KEY REFERENCES users ON DELETE CASCADE,
+    user_id INT PRIMARY KEY REFERENCES users ON DELETE CASCADE
 );
 
 CREATE TABLE tags (
@@ -36,14 +36,14 @@ CREATE TABLE events (
     description TEXT NOT NULL,
     max_participants INT CHECK (max_participants > 0 AND max_participants IS NOT NULL),
     min_age INT CHECK (min_age > 0 AND min_age IS NOT NULL),
-    location_id INT NOT NULL REFERENCES locations ON DELETE CASCADE ON UPDATE,
-    organizer_id INT NOT NULL REFERENCES organizers ON DELETE CASCADE
+    location_id INT REFERENCES locations ON DELETE CASCADE,
+    organizer_id INT REFERENCES organizers ON DELETE CASCADE
 );
 
 CREATE TABLE events_tags (
     event_id INT REFERENCES events ON DELETE CASCADE,
     tag_name VARCHAR(100) REFERENCES tags ON DELETE CASCADE,
-    PRIMARY KEY (event_id, tag_id)
+    PRIMARY KEY (event_id, tag_name)
 );
 
 CREATE TABLE events_users (
