5cfe99474e53d170582d0003ec903f2b4bd99064
Ibrahim <ibrahimelothmani98@gmail.com>
2025-05-15 04:53:06 -1200

    now moving to the PostgreSQL

--- 06-all-tables.sql
+++ 06-all-tables.sql
@@ -1,9 +1,67 @@
-CREATE TABLE cities(
+-- CREATE TABLE cities(
+--     name VARCHAR(200) PRIMARY KEY
+-- );
+
+-- CREATE TABLE locations (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     title VARCHAR(300),
+--     street VARCHAR(300) NOT NULL,
+--     house_number VARCHAR(10) NOT NULL,
+--     postal_code VARCHAR(5) NOT NULL,
+--     city_name VARCHAR(300) NOT NULL REFERENCES cities ON DELETE RESTRICT ON UPDATE CASCADE
+-- );
+
+-- CREATE TABLE users (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     first_name VARCHAR(50) NOT NULL,
+--     last_name VARCHAR(50) NOT NULL,
+--     birthdate DATE NOT NULL,
+--     email VARCHAR(100) NOT NULL
+-- );
+
+-- CREATE TABLE organizers (
+--     password VARCHAR(100) NOT NULL,
+--     user_id INT PRIMARY KEY REFERENCES users ON DELETE CASCADE
+-- );
+
+-- CREATE TABLE tags (
+--     name VARCHAR(100) PRIMARY KEY
+-- );
+
+-- CREATE TABLE events (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     name VARCHAR(300) NOT NULL CHECK (LENGTH(name) > 5),
+--     date_planned TIMESTAMP NOT NULL,
+--     image VARCHAR(300) NULL,
+--     description TEXT NOT NULL,
+--     max_participants INT CHECK (max_participants > 0 AND max_participants IS NOT NULL),
+--     min_age INT CHECK (min_age > 0 AND min_age IS NOT NULL),
+--     location_id INT REFERENCES locations ON DELETE CASCADE,
+--     organizer_id INT REFERENCES organizers ON DELETE CASCADE
+-- );
+
+-- CREATE TABLE events_tags (
+--     event_id INT REFERENCES events ON DELETE CASCADE,
+--     tag_name VARCHAR(100) REFERENCES tags ON DELETE CASCADE,
+--     PRIMARY KEY (event_id, tag_name)
+-- );
+
+-- CREATE TABLE events_users (
+--     event_id INT REFERENCES events ON DELETE CASCADE,
+--     user_id INT REFERENCES users ON DELETE CASCADE,
+--     PRIMARY KEY (event_id, user_id)
+-- );
+
+
+--- POSTGRESQL
+
+
+CREATE TABLE cities (
     name VARCHAR(200) PRIMARY KEY
 );
 
 CREATE TABLE locations (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(300),
     street VARCHAR(300) NOT NULL,
     house_number VARCHAR(10) NOT NULL,
@@ -12,7 +70,7 @@ CREATE TABLE locations (
 );
 
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     birthdate DATE NOT NULL,
@@ -29,13 +87,13 @@ CREATE TABLE tags (
 );
 
 CREATE TABLE events (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(300) NOT NULL CHECK (LENGTH(name) > 5),
     date_planned TIMESTAMP NOT NULL,
-    image VARCHAR(300) NULL,
+    image VARCHAR(300),
     description TEXT NOT NULL,
-    max_participants INT CHECK (max_participants > 0 AND max_participants IS NOT NULL),
-    min_age INT CHECK (min_age > 0 AND min_age IS NOT NULL),
+    max_participants INT NOT NULL CHECK (max_participants > 0),
+    min_age INT NOT NULL CHECK (min_age > 0),
     location_id INT REFERENCES locations ON DELETE CASCADE,
     organizer_id INT REFERENCES organizers ON DELETE CASCADE
 );
